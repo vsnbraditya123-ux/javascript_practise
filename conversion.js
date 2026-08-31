c4998965aa114c2342856906d54a14f2a8dc9424
vsnbraditya-ux <vsnbraditya123@gmail.com>
2026-08-31 18:49:03 +0530

    operations on js

--- conversion.js
+++ conversion.js
@@ -1,27 +1,41 @@
-let score=33
-console.log(typeof score)
-let score1="34"
-console.log(typeof score1);
+// let score=33
+// console.log(typeof score)
+// let score1="34"
+// console.log(typeof score1);
 
-console.log(score1);
+// console.log(score1);
 
 
-let valueInnumber= Number(score1)
-console.log(valueInnumber);
+// let valueInnumber= Number(score1)
+// console.log(valueInnumber);
 
-// "33"=> 33
-//"33abc" => NaN
+// // "33"=> 33
+// //"33abc" => NaN
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn= Boolean(isLoggedIn)
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn);
-//1 => true or 0=> false
-// "" => False 
-//"adi" => True
-let someNumber=33
+// console.log(booleanIsLoggedIn);
+// //1 => true or 0=> false
+// // "" => False 
+// //"adi" => True
+// let someNumber=33
 
-let someString= String(someNumber)
- console.log(typeof someString);
- console.log(someString)
+// let someString= String(someNumber)
+//  console.log(typeof someString);
+//  console.log(someString)
+
+//  ******************* Operations ********************
+
+let value=3
+let negvalue= -value
+console.log(negvalue);
+
+let str1= "Aditya "
+let str2= "vardhan"
+let str3= str1 + str2
+console.log(str3); 
+
+
+console.log(3+"3");
