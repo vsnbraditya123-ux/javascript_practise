50c375d6e5840db165f5c481312e7e2ce66ff3b9
vsnbraditya-ux <vsnbraditya123@gmail.com>
2026-08-31 17:16:35 +0530

    code

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
 console.log("hello")
+const accId=12345
+console.log(accId);
+let accountEmail="aditya123@gmail.com"
+accountpassword="234567890"
+console.table([accId,accountEmail,accountpassword])
