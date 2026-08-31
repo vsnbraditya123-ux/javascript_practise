29d3b4c7f93b14341d1594f401f95eb21bb37ad2
vsnbraditya-ux <vsnbraditya123@gmail.com>
2026-08-31 22:01:20 +0530

    datatype

---
+++ datatypes.js
@@ -0,0 +1,42 @@
+//primitive datatypes
+
+// 7 types
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+
+
+const score=33
+const scorevalue=100.3
+const isLoggedIn=true
+const outsideTemp=null 
+
+let userEmail;
+
+const id=Symbol("123")
+const anotherId=Symbol("123")
+
+
+console.log(id===anotherId);
+// Reference datatypes
+// Arrays
+// Objects
+// Functions
+
+const heros=["shaktiman","naagraj","doga"]
+let user={
+    name:"aditya",
+    age:19,
+    isLoggedIn:true
+}
+
+const myFunction= function(){
+    console.log("hello world");
+}
+
+console.log(typeof myFunction);
