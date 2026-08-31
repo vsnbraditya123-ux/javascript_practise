1a8c16e9e5606a94de22ea10f85a164ba1d7ce46
vsnbraditya-ux <vsnbraditya123@gmail.com>
2026-08-31 18:16:56 +0530

    Conversion of datatypes

---
+++ conversion.js
@@ -0,0 +1,27 @@
+let score=33
+console.log(typeof score)
+let score1="34"
+console.log(typeof score1);
+
+console.log(score1);
+
+
+let valueInnumber= Number(score1)
+console.log(valueInnumber);
+
+// "33"=> 33
+//"33abc" => NaN
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+//1 => true or 0=> false
+// "" => False 
+//"adi" => True
+let someNumber=33
+
+let someString= String(someNumber)
+ console.log(typeof someString);
+ console.log(someString)
