5274865f3cf5896cbe814f8f6554f6a21f182ff3
vsnbraditya-ux <vsnbraditya123@gmail.com>
2026-09-01 12:48:45 +0530

    maths

---
+++ nums&maths.js
@@ -0,0 +1,28 @@
+// const score = 400
+// console.log(score);
+
+// const score1= new Number(200)
+// console.log(score1);
+
+// console.log(score1.toFixed(3));
+// console.log(score1.toPrecision(3));
+
+
+// const hundred=10000000
+// console.log(hundred.toLocaleString('en-IN'))
+
+
+
+// ******************** Maths ************
+
+
+let num=3
+console.log(Math.sqrt(9))
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log(Math.floor(Math.random()*10)+1)
+
+min=34
+max=43
+console.log(Math.floor(Math.random()*(max-min) +1)+min)
